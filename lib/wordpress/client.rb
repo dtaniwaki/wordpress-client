31072beddb57a87834d3d6749a66d10a241809fa
dtaniwaki <daisuketaniwaki@gmail.com>
2013-08-01 15:43:55 -0400

    Add default timeout.

--- lib/wordpress/client.rb
+++ lib/wordpress/client.rb
@@ -28,6 +28,7 @@ Request Dump
         if options.delete(:bearer_token_request) && !access_token.nil?
           http.headers['Authorization'] = bearer_auth_header
         end
+        http.timeout = 10
       end
       res = http.body_str
       
