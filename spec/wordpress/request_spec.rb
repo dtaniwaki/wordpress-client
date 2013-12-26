1d897790d4484d8fbf1329d8408dd45b3ea35a0f
dtaniwaki <daisuketaniwaki@gmail.com>
2013-12-26 00:08:51 -0500

    Add spec test for request.

--- spec/wordpress/request_spec.rb
+++ spec/wordpress/request_spec.rb
@@ -2,4 +2,14 @@ require "spec_helper"
 require 'wordpress/request'
 
 describe Wordpress::Request do
+  let :request do
+    Wordpress::Request.new(:get, 'url')
+  end
+
+  it "should deep clone" do
+    new_request = request.dup
+    new_request.should_not equal request
+    new_request.params.should_not equal request.params
+    new_request.body.should_not equal request.body
+  end
 end
