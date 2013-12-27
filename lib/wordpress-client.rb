8ed67253d283a415a6f981b06cfa0b087b4348cc
dtaniwaki <daisuketaniwaki@gmail.com>
2013-12-26 22:42:12 -0500

    Support require_relative in ruby 1.8.7.

--- lib/wordpress-client.rb
+++ lib/wordpress-client.rb
@@ -1,3 +1,20 @@
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(relative_path)
+      desired_path = File.expand_path('../' + relative_path.to_str, caller[0])
+      shortest = desired_path
+      $:.each do |path|
+        path += '/'
+        if desired_path.index(path) == 0
+          candidate = desired_path.sub(path, '')
+          shortest = candidate if candidate.size < shortest.size
+        end
+      end
+      require shortest
+    end
+  end
+end
+
 require 'logger'
 require 'wordpress/version'
 require 'wordpress/client'
