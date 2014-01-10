3a3b81f104d0fbd7eacaadc4829ae73170f9783a
dtaniwaki <daisuketaniwaki@gmail.com>
2014-01-10 13:59:45 -0500

    Fix wordpress user attributes.

--- lib/wordpress/object/user.rb
+++ lib/wordpress/object/user.rb
@@ -2,7 +2,7 @@ require_relative 'meta_object'
 
 module Wordpress::Object
   class User < MetaObject
-    attr_parameter :id, :display_name, :username, :email, :primary_blog, :avatar_url, :profile_url, :verified
+    attr_parameter :id, :display_name, :username, :email, :primary_blog, :token_site_id, :avatar_url, :profile_url, :verified
     attr_meta_object :site => 'Site'
   end
 end
